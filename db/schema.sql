586fc93d53e0c2e719b6bc023e1c8212afdeb3d3
Ann Newman <anewman1822@gmail.com>
2019-06-28 20:42:28 -0500

    updated database name in schema file

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,6 @@
 -- Drops the inventory_db if it exists currently --
-DROP DATABASE IF EXISTS inventory_db;
+DROP DATABASE IF EXISTS inventory_pro;
 -- Creates the "inventory_db" database --
-CREATE DATABASE inventory_db;
+CREATE DATABASE inventory_pro;
 
 
